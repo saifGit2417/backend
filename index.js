b5c73ae55ce0619d1171c738fed10bbacae15311
saifGit2417 <saifwork2417@gmail.com>
2024-04-15 01:50:25 +0530

    plain get call

--- index.js
+++ index.js
@@ -8,6 +8,12 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/v1", indexRouter);
 
+app.get("/", (req, res) => {
+  res.send(
+    "thi is dummy message from node js application runing on ec2 instances"
+  );
+});
+
 app.listen(PORT_NUMBER ? PORT_NUMBER : 3000, () => {
   console.log(`port running at ${PORT_NUMBER}`);
 });
